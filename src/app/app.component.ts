14aff1df1e4b3d8d802f05871e1614d00a62ab87
dayvesoliveira <dayvesoliveira@gmail.com>
2017-07-04 17:20:32 -0300

    testes com componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RSVendas';
-  menuItens = [{name: 'Vendas'},
-               {name: 'App'},
-               {name: 'Configuraces'}];
-  menuItensConf = [
-               {name: 'Vendas'},
-               {name: 'App'},
-               {name: 'Configuraces'}];
+
+      title = 'RSVendas';
+
+      menuItens = [{name: 'Vendas'},
+                  {name: 'App'},
+                  {name: 'Configuraces'}];
+      
+      menuItensConf = [
+                  {name: 'Vendas'},
+                  {name: 'App'},
+                  {name: 'Configuraces'}];
+
+
+      tiles: any[] = [
+        {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+        {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+        {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+        {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+      ];
+
+      dogs: Object[] = [
+        { name: 'Porter', human: 'Kara' },
+        { name: 'Mal', human: 'Jeremy' },
+        { name: 'Koby', human: 'Igor' },
+        { name: 'Razzle', human: 'Ward' },
+        { name: 'Molly', human: 'Rob' },
+        { name: 'Husi', human: 'Matias' },
+      ];
+
+      basicRowHeight = 80;
+      fixedCols = 4;
+      fixedRowHeight = 100;
+      ratioGutter = 1;
+      fitListHeight = '400px';
+      ratio = '4:1';
+
+      addTileCols() { this.tiles[2].cols++; }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,81 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
-import { MdToolbarModule, MdButtonModule } from '@angular/material';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { 
+  FullscreenOverlayContainer,
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+  OverlayContainer,
+  StyleModule } from '@angular/material';
+
+/**
+ * NgModule that includes all Material modules that are required to serve the demo-app.
+ */
+@NgModule({
+  exports: [
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdDatepickerModule,
+    MdDialogModule,
+    MdExpansionModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdCoreModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSlideToggleModule,
+    MdSliderModule,
+    MdSnackBarModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+    MdNativeDateModule,
+    StyleModule
+  ]
+})
+export class RsMaterialModule {}
 
 @NgModule({
   declarations: [
@@ -14,8 +85,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    MdToolbarModule,
-    MdButtonModule
+    RsMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
